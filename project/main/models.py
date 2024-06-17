566c7d4763ff4c96e4820b2b90a76c30b714466e
che-hyeon <ghkdcogus03@naver.com>
2024-06-17 13:23:10 +0900

    add mentor app

--- project/main/models.py
+++ project/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Mentor(models.Model):
+    mentor_company = models.CharField(max_length=50)
+    mentor_dept = models.CharField(max_length=50)
+    mentor_work = models.CharField(max_length=50)
+    
+    mentor_intro = models.CharField(max_length=300)
+    mentor_year = models.IntegerField()
+
+    mentor_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.mentor_company} - {self.mentor_dept} : {self.mentor_intro[:30]}..."
--- project/main/views.py
+++ project/main/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Mentor
 
 # Create your views here.
+def mainpage(request):
+    return render(request, 'main/mainpage.html')
+
+def mentor_start(request):
+    return render(request, 'main/mentor_start.html')
+
+def mentor_list(request):
+    mentors = Mentor.objects.all()
+    return render(request, 'mentor_list.html', {'mentors' : mentors})
+
+def mentor_info(request, id):
+    mentor = get_object_or_404(Mentor, pk = id)
+    return render(request, 'mentor_info.html', {'mentor' : mentor})
