56b9f4ed39eb3918f83ee2d76373251641395ce1
sunwoo park <sunwoo030616@dgu.ac.kr>
2024-06-18 19:43:03 +0900

    mypage

--- project/users/views.py
+++ project/users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
 
 # Create your views here.
+def mypage(request, id):
+    user = get_object_or_404(User, pk=id)
+    context = {
+        'target_user':user
+    }
+    return render(request, 'users/mypage.html', context)
