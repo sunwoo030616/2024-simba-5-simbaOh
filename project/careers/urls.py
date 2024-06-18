6a586f9fc1e5e0f451da89ce2bd37539e0853aa4
sunwoo park <sunwoo030616@dgu.ac.kr>
2024-06-18 17:50:34 +0900

    career info

---
+++ project/careers/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from main import views
+from .views import *
+
+urlpatterns = [
+    path('new-info', new_info, name="new-info"),
+    path('create', create, name="create"),
+    path('career-info', career_info, name="career-info"),
+    path('<int:id>', detail, name="detail"),
+    path('edit/<int:id>', edit, name="edit"),
+    path('delete/<int:id>', delete, name="delete"),
+]
