2f2a35bd6ce65c6734de55a0770c53a44bbef54d
sunwoo park <sunwoo030616@dgu.ac.kr>
2024-06-19 12:57:40 +0900

    signup분리

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -26,31 +26,97 @@ def logout(request):
 
 def signup(request):
     if request.method == 'POST':
-        user_id = request.POST.get('user_id')
-        user_pw = request.POST.get('user_pw')
+        user_name = request.POST['user_name']
+        user_phone = request.POST['user_phone']
+        user_birth = request.POST['user_birth']
+
+        request.session['user_name'] = user_name
+        request.session['user_phone'] = user_phone
+        request.session['user_birth'] = user_birth
+
+        return redirect('signup2')
+    return render(request, 'accounts/signup.html')
+
+def signup2(request):
+    if request.method == 'POST':
+        user_major = request.POST['user_major']
+        user_enroll = request.POST['user_enroll']
+        user_profile = request.FILES['user_profile']
+
+        request.session['user_major'] = user_major
+        request.session['user_enroll'] = user_enroll
+        request.session['user_profile'] = user_profile
+
+        return redirect('signup3')
+
+    return render(request, 'accounts/signup2.html')
+
+def signup3(request):
+    if request.method == 'POST':
+        user_id = request.POST['user_id']
+        user_pw = request.POST['user_pw']
+
         if request.POST['user_pw'] == request.POST['confirm']:
-                if User.objects.filter(username=user_id).exists():
-                    return render(request, 'accounts/signup.html', {'error': 'Username already exists'})
-                user = User.objects.create_user(
-                    username=request.POST['user_id'],
-                    password=request.POST['user_pw']
+            if User.objects.filter(username=user_id).exists():
+                return render(request, 'accounts/signup.html', {'error': 'Username already exists'})
+    
+            user_name = request.session.get('user_name')
+            user_phone = request.session.get('user_phone')
+            user_birth = request.session.get('user_birth')
+            user_major = request.session.get('user_major')
+            user_enroll = request.session.get('user_enroll')
+            user_profile = request.session.get('user_profile')
+
+            
+            user = User.objects.create_user(
+                username=request.POST['user_id'],
+                password=request.POST['user_pw']                
                 )
-                user_name = request.POST['user_name']
-                user_phone = request.POST['user_phone']
-                user_birth = request.POST['user_birth']
-                user_enroll = request.POST['user_enroll']
+            profile = Profile(
+                user=user,
+                user_name=user_name,
+                user_phone=user_phone,
+                user_birth=user_birth,
+                user_major=user_major,
+                user_enroll=user_enroll,
+                user_profile=user_profile
+            )
+            profile.save()
+
+            auth.login(request, user)
+            return redirect('main:mainpage')
+
+    return render(request, 'accounts/signup3.html')
+
+
+# def signup(request):
+#     if request.method == 'POST':
+#         user_id = request.POST.get('user_id')
+#         user_pw = request.POST.get('user_pw')
+#         if request.POST['user_pw'] == request.POST['confirm']:
+#                 if User.objects.filter(username=user_id).exists():
+#                     return render(request, 'accounts/signup.html', {'error': 'Username already exists'})
+#                 user = User.objects.create_user(
+#                     username=request.POST['user_id'],
+#                     password=request.POST['user_pw']
+#                 )
+#                 user_name = request.POST['user_name']
+#                 user_phone = request.POST['user_phone']
+#                 user_birth = request.POST['user_birth']
+#                 user_enroll = request.POST['user_enroll']
                 
     
                 
-                profile = Profile(user=user,
-                                user_name=user_name,
-                                user_phone=user_phone,
-                                user_birth=user_birth,
-                                user_enroll=user_enroll
-                                )
-                profile.save()
+#                 profile = Profile(user=user,
+#                                 user_name=user_name,
+#                                 user_phone=user_phone,
+#                                 user_birth=user_birth,
+#                                 user_enroll=user_enroll
+#                                 )
+#                 profile.save()
 
-                auth.login(request, user)
-                return redirect('main:mainpage')
+#                 auth.login(request, user)
+#                 return redirect('main:mainpage')
         
-    return render(request, 'accounts/signup.html')
+#     return render(request, 'accounts/signup.html')
+
