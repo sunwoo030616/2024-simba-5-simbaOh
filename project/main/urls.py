aeccc6683b610df2d768dbd177c353f13820409f
che-hyeon <ghkdcogus03@naver.com>
2024-06-21 03:35:53 +0900

    [update] mentor_enroll

--- project/main/urls.py
+++ project/main/urls.py
@@ -11,14 +11,8 @@ urlpatterns = [
     path('mentor-start', mentor_start, name='mentor-start'),
     path('mentor-list/', mentor_list, name='mentor-list'),
     path('mentor-info/<int:id>/', mentor_info, name='mentor-info'),
-    path('mentor-enroll1/', mentor_enroll, name='mentor-enroll'),
-    path('mentor-enroll2/', mentor_enroll2, name='mentor-enroll2'),
-    path('mentor-enroll3/', mentor_enroll3, name='mentor-enroll3'),
-<<<<<<< HEAD
+    path('mentor-create/', mentor_create, name='mentor-create'),
+    path('mentor-enroll/', mentor_enroll, name='mentor-enroll'),
     path('mentor-ask/<int:id>/', mentor_ask, name='mentor-ask'),
     path('follows/<int:mentor_id>/', follows, name='follows'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-    path('mentor-ask', mentor_ask, name='mentor-ask'),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> 091b6bc55a00ac13302ee2658e9b307a7f6de9a8
