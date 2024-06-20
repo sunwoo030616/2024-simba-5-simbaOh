cf4d8b45a1823d04e70d1666ce6ceaa475ddd0ba
sunwoo park <sunwoo030616@dgu.ac.kr>
2024-06-20 17:40:31 +0900

    try

--- project/careers/urls.py
+++ project/careers/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from main import views
 from .views import *
 
+app_name = "careers"
+
 urlpatterns = [
-    path('new-info', new_info, name="new-info"),
-    path('create', create, name="create"),
-    path('career-info', career_info, name="career-info"),
-    path('<int:id>', detail, name="detail"),
-    path('edit/<int:id>', edit, name="edit"),
-    path('delete/<int:id>', delete, name="delete"),
+    path('new-info/', new_info, name="new-info"),
+    path('create/', create, name="create"),
+    path('career-info/', career_info, name="career-info"),
+    path('detail/<int:id>/', detail, name="detail"),
+    path('edit/<int:id>/', edit, name="edit"),
+    path('delete/<int:id>/', delete, name="delete"),
 ]
