76d38b37a8dd7b6ca59157bfebdeef6111712b9a
sunwoo park <sunwoo030616@dgu.ac.kr>
2024-06-21 22:10:17 +0900

    careerinfoupdate

--- project/careers/models.py
+++ project/careers/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Careerinfo(models.Model):
+
     title = models.CharField(max_length=50)
     company = models.CharField(max_length=30)
     place = models.CharField(max_length=10)
